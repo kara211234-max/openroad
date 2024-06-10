01e8318c51e191b4934799ea422cb64a825ab358
ZhiangWang033 <zhw033@ucsd.edu>
2024-06-10 14:23:29 +0200

    DGRePlAce
    Signed-off-by: ZhiangWang033 <zhw033@ucsd.edu>

--- src/OpenRoad.cc
+++ src/OpenRoad.cc
@@ -57,6 +57,7 @@
 #include "dst/MakeDistributed.h"
 #include "fin/MakeFinale.h"
 #include "gpl/MakeReplace.h"
+#include "gpl2/MakeGpuReplace.h"
 #include "grt/MakeGlobalRouter.h"
 #include "gui/MakeGui.h"
 #include "ifp//MakeInitFloorplan.hh"
@@ -139,6 +140,7 @@ OpenRoad::~OpenRoad()
   deleteOpenRCX(extractor_);
   deleteTritonRoute(detailed_router_);
   deleteReplace(replace_);
+  deleteGpuReplace(gpu_replace_);
   deleteFinale(finale_);
   deleteAntennaChecker(antenna_checker_);
   odb::dbDatabase::destroy(db_);
@@ -194,6 +196,7 @@ void OpenRoad::init(Tcl_Interp* tcl_interp)
   extractor_ = makeOpenRCX();
   detailed_router_ = makeTritonRoute();
   replace_ = makeReplace();
+  gpu_replace_ = makeGpuReplace();
   pdnsim_ = makePDNSim();
   antenna_checker_ = makeAntennaChecker();
   partitionMgr_ = makePartitionMgr();
@@ -219,6 +222,7 @@ void OpenRoad::init(Tcl_Interp* tcl_interp)
   initDbVerilogNetwork(this);
   initIoplacer(this);
   initReplace(this);
+  initGpuReplace(this);
   initOpendp(this);
   initOptdp(this);
   initFinale(this);
